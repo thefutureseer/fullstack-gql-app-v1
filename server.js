fb633fb839d9f0919d7ce234dfcefb79a217fa71
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2022-12-24 15:33:25 -0800

    set up server apollo and express with db

--- server.js
+++ server.js
@@ -1,36 +1,49 @@
+//express for middleware
 const express = require('express');
 const path = require('path');
+//apollo server for gql
 const { ApolloServer } = require('apollo-server-express');
 
 const { typeDefs, resolvers } = require('./schema/index');
+//dotenv to hide files easily
 require('dotenv').config();
 
+//bring in the database
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
+
+//get express running
 const app = express();
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
+//add express middleware to apollo server
 server.applyMiddleware({ app });
 
+//add express middleware to parse incoming requests w/urlencoded payloads based on body-parser
 app.use(express.urlencoded({ extended: false }));
+//parse incoming requests with json payloads based on body-parser
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, './client/build')));
 }
-
+//set up environment on / serve index.html from client side
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
+//open up database
 db.once('open', () => {
+  //give the server a port to listen on
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
+    //gql playground
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
