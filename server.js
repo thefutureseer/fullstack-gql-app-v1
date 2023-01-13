6a9c93acbdf860d265fc83e335f6206ed0a73da4
thefutureseer <danielharrislimos@outlook.com>
2023-01-12 19:40:59 -0800

    switching out of heroku

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 //set up environment on / serve index.html from client side
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
+  res.sendFile(path.join(__dirname, './client/build', 'index.html'));
 });
 
 //open up database
