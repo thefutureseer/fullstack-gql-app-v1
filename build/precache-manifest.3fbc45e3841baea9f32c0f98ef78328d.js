0565c487e1ca6fd27743f57b943bb4c9398fb95e
thefutureseer <danielharrislimos@outlook.com>
2023-01-14 20:33:54 -0800

    finally build built

---
+++ build/precache-manifest.3fbc45e3841baea9f32c0f98ef78328d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3848048ec8790c5136b9",
+    "url": "/static/css/main.294b6a35.chunk.css"
+  },
+  {
+    "revision": "3848048ec8790c5136b9",
+    "url": "/static/js/main.2198f1d1.chunk.js"
+  },
+  {
+    "revision": "2c2c8b07d153ed954905",
+    "url": "/static/js/runtime~main.77ace5e1.js"
+  },
+  {
+    "revision": "aab5460bf0307041fbd9",
+    "url": "/static/js/2.70031965.chunk.js"
+  },
+  {
+    "revision": "283fe33f7690b8d03a74",
+    "url": "/static/js/3.d0bba312.chunk.js"
+  },
+  {
+    "revision": "52557506f5afb1e5c68ce850008970b6",
+    "url": "/index.html"
+  }
+];
