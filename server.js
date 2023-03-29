48f4cba4439163809bc148fe52084cf4e53c3c95
thefutureseer <danielharrislimos@outlook.com>
2023-03-29 13:44:45 -0700

    prodo bld

--- server.js
+++ server.js
@@ -30,9 +30,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, './client/build')));
-}
+//serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+//set static folder
+ app.use(express.static('client/build'));
+ app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+ });
+} 
 //set up environment on / serve index.html from client side
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './client/build', 'index.html'));
