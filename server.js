734c4801975c294760e690da3e23bfce29df5a19
thefutureseer <danielharrislimos@outlook.com>
2023-03-30 18:36:33 -0700

    add public folder to deploy

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(express.json());
 //serve static assets if in production
 if(process.env.NODE_ENV === 'production') {
 //set static folder
- app.use(express.static('client/build'));
+ app.use(express.static('client'));
  app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
  });
