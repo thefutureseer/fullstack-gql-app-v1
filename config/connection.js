e8072622449f9170ae92548cb2a8263a3f97d933
thefutureseer <danielharrislimos@outlook.com>
2023-11-06 14:36:57 -0800

    new setting for mongodb

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
+// Set strictQuery to true to suppress the deprecation warning
+mongoose.set('strictQuery', true);
+
 mongoose.connect(process.env.MONGODB,
   {
     useNewUrlParser: true,
